c4737ef04e247609dbf63f38706c15b2102fea59
Sergii Piatakov <sergii.piatakov@globallogic.com>
2018-11-15 15:28:04 +0200

    fix truncation error and improve calculation accuracy
    To convert float to integer the truncation is performed, but the
    rounding is expected.
    
    Test: Add (4.9, 4.9) should return 10.
    Signed-off-by: Sergii Piatakov <sergii.piatakov@globallogic.com>
    Signed-off-by: Bohdana  <bohdankaaa6@gmail.com>

--- calculator.cpp
+++ calculator.cpp
@@ -2,10 +2,11 @@
 
 int Calculator::Add (int a, int b)
 {
-    return a + b;
+	return a + b + 0.5;
+
 }
 
 int Calculator::Sub (int a, int b)
 {
-    return Add (a, -b);
+	return Add (a, -b);
 }
--- calculator.h
+++ calculator.h
@@ -3,9 +3,15 @@
 
 class Calculator
 {
+<<<<<<< HEAD
     public:
         int Add (int, int);
         int Sub (int, int);
+=======
+	public:
+		int Add (double, double);
+		int Sub (double, double);
+>>>>>>> cd72550 (fix truncation error and improve calculation accuracy)
 };
 
 #endif//CALCULATOR_H
