ae545b63513b208bc6e80842c59f8d136cb72060
Bohdana <bohdankaaa6@gmail.com>
2025-09-29 22:42:23 +0300

    add division operation with zero-check

--- calculator.cpp
+++ calculator.cpp
@@ -2,16 +2,33 @@
 
 int Calculator::Add (int a, int b)
 {
-	return a + b + 0.5;
+	return a + b;
 
 }
 
 int Calculator::Sub (int a, int b)
 {
-	return Add (a, -b);
+	return a - b;
+}
+
+int Calculator::Add (double a, double b)
+{
+	return a + b + 0.5;
+}
+
+int Calculator::Sub (double a, double b)
+{
+	return Add(a, -b);
 }
 
 int Calculator::Mul (double a, double b)
 {
     return a * b;
+
+int Calculator::Div (double a, double b)
+{
+	if (b == 0) {
+		throw std::invalid_argument("Division by zero");
+	}
+	return a / b;
 }
--- calculator.h
+++ calculator.h
@@ -1,17 +1,18 @@
 #ifndef CALCULATOR_H
 #define CALCULATOR_H
 
+#include <stdexcept>
 class Calculator
 {
-    public:
-        int Add (int, int);
+	public:
+        	int Add (int, int);
+        	int Sub (int, int);
 
-        int Sub (int, int);
 	public:
 		int Add (double, double);
 		int Sub (double, double);
-
-        int Mul (double, double);
+		int Mul (double, double);
+		int Div (double, double);//new method
 };
 
 #endif//CALCULATOR_H
